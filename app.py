02328eecbb2159d8b5963408fd041c41230e71a7
RicardoSanPer <ricardosp@ciencias.unam.mx>
2024-02-19 21:59:10 -0600

    App principal

--- app.py
+++ app.py
@@ -8,11 +8,13 @@ from hashlib import sha256
 
 from model.model_alumno import borra_alumno
 
+from utilidad import datosAleatorios
+
 #mysql+pymysql://ferfong:Developer123!@localhost:3306/ing_soft
 #<dialecto>+<driver>://<usuario>:<passwd>@localhost:3306/<db>
 #mysql+pymysql://lab:Developer123!@localhost:3306/lab_ing_soft
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://lab:Developer123!@localhost:3306/lab_ing_software'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://lab:Developer123!@localhost:3306/RSP_Lab_Ing_P2'
 app.config.from_mapping(
     SECRET_KEY='dev'
 )
@@ -20,25 +22,8 @@ db.init_app(app)
 
 if __name__ == '__main__':
     with app.app_context():
-        """for alumno in Alumno.query.all(): # Select * from alumno
-            print(alumno)"""
+        a = datosAleatorios.RandomData("usuarios.json")
+        
+        print(a.GetRandomData("nombre"))
 
-        """for alumno in Alumno.query.filter(and_(Alumno.nombre == 'Fer', Alumno.num_cta == 313320679)): #Un booleano a evaluar.
-            print(f"Nombre de alumno con cta 313320679 es: {alumno.nombre}")"""
-
-        #Create
-        """valeria = Alumno('Valeria', 'Ramirez', 319311918, apMat=None, password=sha256(cipher("Developer123#")).hexdigest())
-        db.session.add(valeria)
-        db.session.commit()"""
-        #Update
-        #Primero tenemos que buscar el objeto que queremos.
-        #Ya que lo tengo, entonces cambio el atributo.
-        #Y entonces hago el commit.
-        #fer = Alumno.query.filter(Alumno.nombre == 'Fernando').first()
-        #print(type(fer))
-        #fer.nombre = "Fer"
-        #fer.ap_mat = "Baeza"
-        #db.session.commit()
-        #Delete
-        borra_alumno(313320679)
         print("Borrado con éxito!")
