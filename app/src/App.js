27d307c5e5f85a53d05ad615049486fe3de76839
RicardoSanPer <ricardosp@ciencias.unam.mx>
2024-03-25 11:26:45 -0600

    Routing y vistas
    Implementada funcionalidad básica para cambiar de vistas y páginas

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+import Navigation from "./components/Navigation"
+import Main from "./components/Main"
+
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <Navigation />
+      <Main />
     </div>
   );
 }
---
+++ app/src/components/Main.js
@@ -0,0 +1,13 @@
+import { NavLink, Route, Routes } from 'react-router-dom';
+import Usuarios from '../pages/Usuarios';
+import Home from "../pages/Home";
+
+const Main = () =>
+(
+    <Routes>
+        <Route path="/" Component={Home}></Route>
+        <Route path="/usuarios" Component={Usuarios}></Route>
+    </Routes>
+);
+
+export default Main;
