4fd5786fbcc756b6b0c625b960594ced19f66880
RicardoSanPer <ricardosp@ciencias.unam.mx>
2024-04-12 18:33:44 -0600

    Datos de muestra renta

---
+++ app/src/data/rentas.js
@@ -0,0 +1,5 @@
+let sample_rentas = [{"idUsuario": 1, "idPelicula": 1, "fecha_renta":"2020-02-25", "dias_de_renta": 3, "estatus":false},
+{"idUsuario": 2, "idPelicula": 2, "fecha_renta":"2024-04-12", "dias_de_renta": 3, "estatus":true},
+{"idUsuario": 3, "idPelicula": 3, "fecha_renta":"2024-02-12", "dias_de_renta": 3, "estatus":false}]
+
+export default sample_rentas;
